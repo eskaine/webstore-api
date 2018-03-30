74bd572b0389ac4f06d370080466a942f93eb181
eskaine <eskaineliu@gmail.com>
2018-03-30 20:05:42 +0800

    Refactor jest tests

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,18 +1,18 @@
 const request = require('supertest');
 const app = require('../server');
 
-//const usersTest = require('./routes/users.test');
-
 describe('test / path', () => {
 	test('should return json from GET method', () => {
-		return request(app).get('/').expect(
-			200, {'message': 'request received!'}
-		);
+		return request(app).get('/')
+			.expect('Content-Type', /json/)
+			.expect(200);
 	});
 });
 
 describe('test users routes', () => {
 	test('should return status 200 from POST method', () => {
-		return request(app).post('/users/signup').expect(200);
+		return request(app).post('/users/signup')
+			.expect('Content-Type', /json/)
+			.expect(200);
 	});
 });
--- __tests__/routes/users.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-
-module.exports = (app) => {
-	describe('test users routes', () => {
-		test('should return status 200 from POST method', () => {
-			return request(app).post('/users/signup').expect(200);
-		});
-	});
-};
