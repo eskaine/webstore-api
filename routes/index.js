e4bf32d1c9205e7fbb4259eb2e15b5aab264ffb9
eskaine <eskaineliu@gmail.com>
2018-03-31 15:51:22 +0800

    Added error handling

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,21 @@ module.exports = (app) => {
 			message: 'request received!'
 		});
 	});
+
+	//Catch all unavailable routes
+	app.use((req, res, next) => {
+		const error = new Error('Not found');
+		error.status = 404;
+		next(error);
+	});
+
+	//Catch all other errors
+	app.use((error, req, res, next) => {
+		res.status(error.status || 500);
+		res.json({
+			error: {
+				message: error.message
+			}
+		});
+	});
 };
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
+const morgan = require('morgan');
+
 const app = express();
 
 const routes = require('./routes/index');
 
 app.use(helmet());
+app.use(morgan('dev'));
 
 routes(app);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
