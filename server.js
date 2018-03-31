f927a6e87693262fe6b849c2eacabecd0d492e97
eskaine <eskaineliu@gmail.com>
2018-03-31 17:52:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -8,6 +9,18 @@ const routes = require('./routes/index');
 
 app.use(helmet());
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+	if(req.method === 'OPTIONS') {
+		res.header('Access-Control-Allow-Methods', 'Put, POST, PATCH, DELETE, GET');
+		return res.status(200).json();
+	}
+	next();
+});
 
 routes(app);
 
