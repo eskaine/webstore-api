a8053316a341aef3914981a18c9e348262ac9129
eskaine <eskaineliu@gmail.com>
2018-04-01 21:15:00 +0800

    Update passport.js

--- auth/passport.js
+++ auth/passport.js
@@ -1,9 +1,10 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/users');
 
-module.exports = (passport) => {
+module.exports = () => {
 	passport.serializeUser(function(user, done) {
 		done(null, user.id);
 	});
@@ -14,14 +15,31 @@ module.exports = (passport) => {
 		});
 	});
 
-	passport.use(new LocalStrategy({
+	passport.use('login', new LocalStrategy({
 		usernameField: 'email',
 		passwordField: 'password',
-		session: false
-	},
-	function(email, password, done) {
-		// ...
-	}
-	));
+		session: false,
+		passReqToCallback: true
+	}, (req, email, password, done) => {
+		process.nextTick(() => {
+			User.find({ email: req.body.email }, (err, user) => {
+				if(err)
+					return done(err);
+
+				if(!user)
+					return done(null, false);
+
+				bcrypt.compare(password, user.password, (err, result) => {
+					if(err)
+						return done(err);
+
+					if(!result)
+						return done(null, false);
+
+					return done(null, user);
+				});
+			});
+		});
+	}));
 
 };
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,11 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 router.post('/signup', (req, res, next) => {
+
+
 	//to handle passport signup
 	res.status(200).json({
 		message: 'Signup successful!'
@@ -8,10 +13,24 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', (req, res, next) => {
-	//to handle passport login
-	res.status(200).json({
-		message: 'Login successful!'
-	});
+
+	passport.authenticate('login', { session: false }, (err, user, info) => {
+
+		if(err || !user) {
+			return res.status(400).json({
+				message: 'invalid',
+				user: user
+			});
+		}
+
+		req.login(user, { session: false }, (err) => {
+			if(err) res.send(err);
+
+			const token = jwt.sign(user, process.env.JWT_KEY);
+			return res.json({user, token});
+		});
+
+	})(req, res);
 });
 
 module.exports = router;
