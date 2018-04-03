c3ccf0722b307e3b9a5cb88500d0b13971e892c4
eskaine <eskaineliu@gmail.com>
2018-04-03 20:00:29 +0800

    Refactor users.js

--- routes/users.js
+++ routes/users.js
@@ -3,41 +3,40 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 
-router.post('/signup', passportCallback('signup'));
-router.post('/login', passportCallback('login'));
+const passportCallback = (req, res, next) => {
+	const path = req.route.path.match(/[a-zA-Z]+/g)[0];
+	passport.authenticate(path, { session: false }, (err, user) => {
+		if(err)
+			return next(err);
 
-function passportCallback(path) {
-	return (req, res, next) => {
-		passport.authenticate(path, { session: false }, (err, user) => {
-			if(err)
-				return next(err);
-
-			if(!user) {
-				return res.status(400).json({
-					message: 'invalid',
-					user: user
-				});
-			}
-
-			req.login(user, { session: false }, (err) => {
-				if(err)
-					res.send(err);
-
-				const token = jwt.sign(
-					{
-						email: user.local.email
-					},
-					process.env.JWT_KEY,
-					{ expiresIn: '1h' }
-				);
-
-				return res.status(200).json({
-					message: 'Authentication successful',
-					token: token
-				});
+		if(!user) {
+			return res.status(400).json({
+				message: 'invalid',
+				user: user
 			});
-		})(req, res);
-	};
-}
+		}
+
+		req.login(user, { session: false }, (err) => {
+			if(err)
+				res.send(err);
+
+			const token = jwt.sign(
+				{
+					email: user.local.email
+				},
+				process.env.JWT_KEY,
+				{ expiresIn: '1h' }
+			);
+
+			return res.status(200).json({
+				message: 'Authentication successful',
+				token: token
+			});
+		});
+	})(req, res);
+};
+
+router.post('/signup', passportCallback);
+router.post('/login', passportCallback);
 
 module.exports = router;
