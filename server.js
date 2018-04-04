8bde22f69d10cb3a7065ef149d794046f71916b0
eskaine <eskaineliu@gmail.com>
2018-04-04 15:08:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const RateLimit = require('express-rate-limit');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -12,10 +13,17 @@ const app = express();
 const routes = require('./routes/index');
 require('./auth/passport')(passport);
 
+const limiter = new RateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+	delayMs: 0
+});
+
 mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
 
 app.use(helmet());
+app.use(limiter);
 app.use(morgan('dev'));
 app.use(passport.initialize());
 app.use(passport.session());
